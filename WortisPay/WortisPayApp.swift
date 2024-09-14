ba73493f7903e74fc104144b99767f28a663913c
g2on60 <128988560+g2on60@users.noreply.github.com>
2024-09-14 14:24:28 +0100

    Add files via upload

---
+++ WortisPay/WortisPayApp.swift
@@ -0,0 +1,17 @@
+//
+//  WortisPayApp.swift
+//  WortisPay
+//
+//  Created by Wortis on 04/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct WortisPayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
